f0ce967382f2a705677738c17995a0b0562dbb67
Mark D. Blackwell <markdblackwell01@gmail.com>
2018-09-26 15:17:05 -0400

    Add version

---
+++ lib/qplaylist_runner/version.rb
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+=begin
+Copyright (C) 2018 Mark D. Blackwell.
+   All rights reserved.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+=end
+
+module ::QplaylistRunner
+  VERSION = '1.0.0'
+end
